2e956babeb36c4386a8b1827c66b48ca33b5bf50
pedro <pedrokoberstain2@gmail.com>
2024-08-05 16:18:36 -0400

    commit initializr

---
+++ src/main/java/com/compassuol/api_gateway/RestWithSpringBootAndJavaKoberApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaKoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaKoberApplication.class, args);
+	}
+
+}
